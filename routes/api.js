b0229bf7be343906be7157fb7d71a50e773af233
UwUBeebi <chris.erman@khk.ee>
2022-04-01 13:11:52 +0300

    Created API for tags information by tag name and id

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,12 @@ const router = express.Router();
 // define article controller and export it for this file
 const apiArticleController = require('../controllers/api/article');
 const apiAuthorController = require('../controllers/api/author');
+const apiTagController = require('../controllers/api/tags')
 
 // use controller functions according to the route
 router.get('/article', apiArticleController.getArticleBySlug);
 router.get('/author', apiAuthorController.getAuthor);
+router.get('/tag', apiTagController.getTags);
 
 // export article router for using in default application file
 module.exports = router;
